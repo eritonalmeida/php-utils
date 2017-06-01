ca5086e920b14e3f995893a4d5f0cf2761992276
Eriton Almeida <eriton@mundilivre.com.br>
2017-05-31 23:41:04 -0300

    Update mongodb-auto_increment.php

--- mongodb-auto_increment.php
+++ mongodb-auto_increment.php
@@ -8,7 +8,7 @@ $db = new MongoDB\Client();
 $database = 'my_database';
 $db->selectDatabase($database);
 
-$counters = $db->selectCollection('my_counters', $database);
+$counters = $db->selectCollection($database, 'my_counters');
 $filter = array('id' => 'users');
 $update = array('$inc' => array('counter' => 1));
 $options = array('returnDocument' => 2, 'upsert' => true);
@@ -17,7 +17,7 @@ $options = array('returnDocument' => 2, 'upsert' => true);
 $result = $counters->findOneAndUpdate($filter, $update, $options);
 
 //save new user
-$users = $db->selectCollection('users', $database);
+$users = $db->selectCollection($database, 'users');
 $data = array(
     'id' => $result->counter,
     'name' => 'Test'
